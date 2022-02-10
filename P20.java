d7f04832514f78e61189b36785f70bf4241469bc
Aashi1996 <98079307+Aashi1996@users.noreply.github.com>
2022-02-10 13:16:04 +0530

    Add files via upload

---
+++ P20.java
@@ -0,0 +1,13 @@
+
+public class P20 {
+
+	public static void main(String[] args) {
+		
+		for(int num=1; num<=200; num++) {
+			int multiple = 9 * num;
+			System.out.print(multiple + " ");
+			}
+
+	}
+
+}
---
+++ P21.java
@@ -0,0 +1,13 @@
+
+public class P21 {
+
+	public static void main(String[] args) {
+		
+		int i, j;
+		for (i = 0, j = 0; i * j < 1000; i++, j += 2) {
+		 System.out.println(i + " * " + j + " = " + (i * j));
+		}
+
+	}
+
+}
